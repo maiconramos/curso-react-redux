fd4ba2e50508e9aa5648dadf17208ca6fe146dbf
maiconramos <maiconx@outlook.com>
2019-06-11 07:40:04 -0300

    Novos exercios

--- exercicios_react/component-in-class/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ClassComponent from './classComponenet'
-
-ReactDOM.render(
-    <ClassComponent value='Componente Classe' />
-    , document.getElementById('app'))
-
--- exercicios_react/webpack.config.js
+++ exercicios_react/webpack.config.js
@@ -10,16 +10,18 @@ module.exports = {
         port: 8080,
         contentBase: './public',
     },
+    // Adicionado no ex.3
     resolve: {
-      extensions: ['' , '.js' , ['.jsx']]
+        extensions: ['', '.js', '.jsx']
     },
     module: {
         loaders: [{
-            test: /.jsx?$/,
+            test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                presets: ['es2015' , 'react'],
+                presets: ['es2015', 'react'],
+                // O plugin foi adicionado no ex.12
                 plugins: ['transform-object-rest-spread']
             }
         }]
