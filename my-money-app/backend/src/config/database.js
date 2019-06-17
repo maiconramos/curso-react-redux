5827deea7e604d2eafdf79036734ec1ffd66b40c
maiconramos <maiconx@outlook.com>
2019-06-17 08:16:18 -0300

    fixed useNewUrlParser

--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mymoney
-module.exports = mongoose.connect(url, { useMongoClient: true })
+const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mymoney'
+module.exports = mongoose.connect(url)
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = 
