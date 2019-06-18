effe6d1c2edfca2069913f7ebd51e3ded4484aa5
maiconramos <maiconx@outlook.com>
2019-06-18 08:26:30 -0300

    first commit frontend

--- my-money-app/frontend/src/consts.js
+++ my-money-app/frontend/src/consts.js
@@ -1,4 +1,12 @@
+/* Local
 export default {
     API_URL: 'http://localhost:3003/api',
     OAPI_URL: 'http://localhost:3003/oapi',
+}*/
+
+/* Heroku */
+export default {
+    API_URL: 'https://financeiro-maicon.herokuapp.com/api',
+    OAPI_URL: 'https://financeiro-maicon.herokuapp.com/oapi',
 }
+
